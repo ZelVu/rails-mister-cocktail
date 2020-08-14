268ed299ace1d893e04d531ce372646f0f317acc
Zeljka Vujic <vujic.zeljka@outlook.de>
2020-08-14 12:34:37 +0200

    Some changes in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  
-  resources :cocktails do
+  root to:"cocktails#index"
+  resources :cocktails, except: [ :index ] do
 
     resources :doses, only: [ :create ]
   end
