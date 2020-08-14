0d99b4ed7fe11d1bddb7db1f676d04b6f4ea1f62
Zeljka Vujic <vujic.zeljka@outlook.de>
2020-08-14 12:28:19 +0200

    routes changed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -22,7 +22,9 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails do 
-    resources :doses#, only: [ :new, :create, :destroy ]
+  
+  resources :cocktails do
+
+    resources :doses, only: [ :create ]
   end
   resources :doses, only: [:destroy]
 end
